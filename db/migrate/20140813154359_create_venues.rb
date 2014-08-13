6706ce2dab4df0732ada688efcbfb2286c44c04d
Matt Collier <matt.collier@hotmail.com>
2014-08-13 16:47:46 +0100

    set up venue model

---
+++ db/migrate/20140813154359_create_venues.rb
@@ -0,0 +1,12 @@
+class CreateVenues < ActiveRecord::Migration
+  def change
+    create_table :venues do |t|
+      t.string :title
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
