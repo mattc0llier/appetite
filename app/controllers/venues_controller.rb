1999d96160b81cdea890243eb890919a0d1c70c1
Matt Collier <matt.collier@hotmail.com>
2014-08-13 17:11:46 +0100

    new view and started create action

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,11 +1,22 @@
 class VenuesController < ApplicationController
 
 	def index
+		# @venues = "where teh venues will go"
+		@venues = Venue.all
 	end
 
 	def new
+		@venue = Venue.new
 	end
 
 	def create
+		@venue = Venue.new(venue_params)
+		@venue.save
 	end
+
+	private
+	def venue_params
+		params.require(:venue).permit(:title, :address, :longitude, :latitude)
+	end
+
 end
